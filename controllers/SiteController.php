b161aae60f7a77c6e04adee20b8b4910b8ce3db7
lianghao.tang <lianghao.tang@weicheche.cn>
2016-05-10 19:23:24 +0800

    datatable插件配置

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,3 +1,4 @@
+
 <?php
 class SiteController extends Controller
 {
@@ -8,7 +9,49 @@ class SiteController extends Controller
         $this->assign('hello', $hello);
         $this->display();
     }
-    
+
+    public function dataTable()
+    {
+        $this->display();
+    }
+
+    /**
+     * datatable 异步调用
+     * @return [json] json数组
+     * @author pangxieke
+     * @date   2016-05-10
+     */
+    public function ajaxTable()
+    {
+        $start = $_GET['start']; //显示的起始索引
+        $length = $_GET['length'];//显示的行数
+
+        //搜索条件
+        $status = $_GET['status'];
+        $where = ['status'=>$status];
+
+        $res = [];
+        $aaData = [];
+        for($i=0; $i<=20; $i++){
+            $res[] = [
+                $i,
+                'username',
+                'tousername',
+                time(),
+                'status'
+            ];
+        }
+        $aaData = array_slice($res, $start, $length);
+
+        $data = [];
+        $data['sEcho'] = $_GET['sEcho'];
+        $data['iTotalDisplayRecords'] = count($res);
+        $data['iTotalRecords'] = count($res);
+        $data['aaData'] = $aaData;
+        $data['iDisplayLength'] = $length;
+        echo json_encode($data);
+    }
+
     public function hello()
     {
         echo 'hello world!';
