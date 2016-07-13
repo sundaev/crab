b84aae507907324390ac131e45222781131bded3
lhta <lhta@royole.com>
2016-07-13 19:58:55 +0800

    修改core文件夹名为src

---
+++ src/Core.php
@@ -0,0 +1,31 @@
+<?php
+//配置文件
+require CORE_PATH . DS . 'Config.php';
+
+//公共函数文件
+// require CORE_PATH . DS . 'Common.php';
+
+//基类
+// require CORE_PATH . DS . 'Base.php';
+
+//包含Smarty类文件
+require CORE_PATH . DS . 'smarty/Smarty.class.php';
+
+// //控制器
+// require CORE_PATH . DS . 'Controller.php';
+
+// //数据库
+// require CORE_PATH . DS . 'Db' . DS . 'Db.php';
+
+// //模型
+// require CORE_PATH . DS . 'Model.php';
+
+
+//加载类库加载类
+//require CORE_PATH . DS . 'Loader.php';
+
+/*预加载系统内置类*/
+//require CORE_PATH .DS.'import.php';
+
+/*系统常量配置*/
+//require CORE_PATH .DS.'defines.php';
